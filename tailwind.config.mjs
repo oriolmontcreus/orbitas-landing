b5e8f7b6acdd265040338eff826f8bb40b7a21a3
oriolmontcreus <licensedgamer641319@gmail.com>
2024-10-20 16:45:47 +0200

    feat: added ripple effect

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,16 +3,25 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
-			animation: {
-				"shine": "shine 2s linear infinite"
+		  animation: {
+			"shine": "shine 2s linear infinite",
+			"ripple": "ripple var(--duration,2s) ease calc(var(--i, 0)*.2s) infinite"
+		  },
+		  keyframes: {
+			"shine": {
+			  "from": { "backgroundPosition": "0 0" },
+			  "to": { "backgroundPosition": "-200% 0" }
 			},
-			keyframes: {
-				"shine": {
-					"from": { "backgroundPosition": "0 0" },
-					"to": {	"backgroundPosition": "-200% 0" }
-				}
+			"ripple": {
+			  "0%, 100%": {
+				transform: "translate(-50%, -50%) scale(1)",
+			  },
+			  "50%": {
+				transform: "translate(-50%, -50%) scale(0.9)",
+			  },
 			}
+		  }
 		},
-	},
+	  },
 	plugins: [],
 }
